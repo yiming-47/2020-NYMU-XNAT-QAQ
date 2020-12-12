27768d95c5f1ca85dc7c5524a6cd4d3f976caec9
yiming-47 <75118272+yiming-47@users.noreply.github.com>
2020-12-12 22:23:07 +0800

    Update upload.py

--- upload.py
+++ upload.py
@@ -2,10 +2,11 @@ import xnat
 import os 
 import glob
 import pyxnat
+import pandas as pd
 # coding: utf-8
 import zipfile
-
-path = './CGRD'
+import time
+path = "D:/XNat"
 # zipfile example
 def zip_dir(path):
     zf = zipfile.ZipFile('{}.zip'.format(path), 'w', zipfile.ZIP_DEFLATED)
@@ -13,9 +14,36 @@ def zip_dir(path):
         for file_name in files:
             zf.write(os.path.join(root, file_name))
 
+def read_CTMR(path):
+    df = pd.read_csv(path + '/CTMR.csv')
+    img_path = path + '/CGRD/' + df['fullpath']
+    return img_path
+
 
 if __name__ == '__main__':
-    #for filename in os.listdir(path):
+    img_path = read_CTMR(path)
+    session = xnat.connect('http://120.126.47.114', user='admin', password='admin123')
+    for i, patientID in enumerate(img_path):
+        time.sleep(180) #3mins
+        try:
+            zip_dir(patientID)
+            df = pd.read_csv(path + '/CTMR.csv')
+            print(i,patientID)
+            prearchive_session = session.services.import_(patientID + '.zip', project='109_TM', subject=df['fullpath'][i], destination='/prearchive')
+            #print(prearchive_session)
+            #session.prearchive.sessions()
+            prearchive_session = session.prearchive.sessions()[0]
+            experiment = prearchive_session.archive(subject=df['fullpath'][i], experiment=df['fullpath'][i])
+             
+        except:
+            print('error')  
+            pass
+        continue
+    session.disconnect()
+
+    ##------------------------------------------------------------------------------------------------------
+    # session.disconnect()
+    # for filename in os.listdir(path):
     #    zip_dir(path + '/' + filename)
     #zip_dir(path)
     # session = pyxnat.Interface('http://120.126.47.114', user='admin', password='admin')
@@ -23,23 +51,24 @@ if __name__ == '__main__':
     # project.create(
     #project.create()
     # session.disconnect()
-    os.chdir('D:/XNat/CGRD')
+    '''os.chdir('D:/XNat/CGRD')
+    # session = xnat.connect('http://120.126.47.114', user='admin', password='admin123')
     for i in glob.glob('*.zip'):
-        try:
-            #os.remove('D:/XNat/CGRD/' + i)
-            patient_name = i[0:len(i) - 4]
-            print(patient_name)
-            session = xnat.connect('http://120.126.47.114', user='admin', password='admin')
-            print('./' + i)
-            prearchive_session = session.services.import_('./' + i, project='109_translation_medical', subject=patient_name, destination='/prearchive')
-            #print(prearchive_session)
-            #session.prearchive.sessions()
-            prearchive_session = session.prearchive.sessions()[0]
-            print(prearchive_session)
-            experiment = prearchive_session.archive(subject=patient_name, experiment=patient_name)
-            print(experiment)
-        except:
-            print('error')
-            pass
-        session.disconnect()
-        continue
+        # try:
+        os.remove('D:/XNat/CGRD/' + i)
+    #         patient_name = i[0:len(i) - 4]
+    #         print(patient_name)
+    #         print('./' + i)
+    #         prearchive_session = session.services.import_('./' + i, project='109_translation_medical', subject=patient_name, destination='/prearchive')
+    #         #print(prearchive_session)
+    #         #session.prearchive.sessions()
+    #         prearchive_session = session.prearchive.sessions()[0]
+    #         print(prearchive_session)
+    #         experiment = prearchive_session.archive(subject=patient_name, experiment=patient_name)
+    #         print(experiment)
+    #     except:
+    #         print('error')
+    #         pass
+    #     continue
+    
+    # session.disconnect()'''
